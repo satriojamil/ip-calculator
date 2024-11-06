a594fad6021723de795c38baeaed2a13e3c70b4c
satriojamil <151606465+satriojamil@users.noreply.github.com>
2024-11-06 13:58:55 +0700

    pemecahan fungsi def ip calculator.py

--- ip calculator.py
+++ ip calculator.py
@@ -1,5 +1,4 @@
 # Input The IP Address and Prefix 
-print("Anggota Kelompok : Jamil, Sabda, Radit")
 print("============================================")
 inputSubnet = int(input("Input Netmask = "))    # Input The IP Address
 inputIP = input("Input IP Address = ")          # Input The Prefix
@@ -10,78 +9,95 @@ def sumIP(x):
     jumlahIP = 2**(32-x)
     return jumlahIP
 
-
-# Function for Calculate The Subnetmask like 255.255.255.0 --> /24
-def subnetting(x):
+# Function for Calculate The Subnetmask Class 'C' like 255.255.255.0 --> /24
+def subnetmaskC(x):
     subnetmask = 256 - x
-    print(f'Subnetmask      = 255.255.255.{subnetmask}')
+    return f'Subnetmask      = 255.255.255.{subnetmask}'
+
+# Function for Calculate The Subnetmask Class 'B' like 255.255.0.0 --> /16
+def subnetmaskB(x):
+    prefix = inputSubnet + 8
+    subnetmask = 256 - (sumIP(prefix))
+    return f'Subnetmask      = 255.255.{subnetmask}.0'
 
 
-# Function for Calculate Network and Broadcast Address from The Particular Network
-def network_broadcast_IP(x):
+# Function for Calculate Network Address from The Particular Network
+def networkIP(x):
     Networks = x.split('.')
     Network = int(Networks[3])
-    if Network < jumlahIP:
+    sumip = sumIP(inputSubnet)
+    if Network < sumip:
         Networks.insert(3,0)
         Networks.pop()
-        Network = int(Networks[3])
-        Network = int(Networks[3])
         octetNet = ""
         for i in Networks:
             octetNet += (str(i) + '.')
-        print(f"Network IP      = {octetNet[:-1]}")
+        return octetNet[:-1]
     else:
-        operation = (int(Networks[3]) // jumlahIP) * jumlahIP
+        operation = (int(Networks[3]) // sumip) * sumip
         Networks.insert(3,operation)
         Networks.pop()
-        Network = int(Networks[3])
-        Network = int(Networks[3])
         octetNet = ""
         for i in Networks:
             octetNet += (str(i) + '.')
-        print(f"Network IP      = {octetNet[:-1]}")
+        return octetNet[:-1]
+network = networkIP(inputIP)
 
-    Broadcast = Network + jumlahIP - 1
-    Networks.insert(3,Broadcast)
-    Networks.pop()
+
+# Function for Calculate Broadcast Address from The Particular Network
+def broadcastIP(x):
+    Network = x.split('.')
+    Networks = int(Network[3])
+    sumip = sumIP(inputSubnet)
+    Broadcast = Networks + sumip - 1
+    Network.insert(3,Broadcast)
+    Network.pop()
     octetBroad = ''
-    for i in Networks:
+    for i in Network:
         octetBroad += (str(i) + '.')
-    print(f"Broadcast IP    = {octetBroad[:-1]}")
+    return octetBroad[:-1]
+broadcast = broadcastIP(network)
 
-    host_bottom = Networks
-    host_top = host_bottom
-    bottomIP = Network + 1
-    topIP = Broadcast - 1
+def rangeIP(first,last):
+    firstRange = first.split('.')
+    lastRange = last.split('.')
 
-    host_bottom.insert(3,bottomIP)
-    host_bottom.pop()
-    octetHostBottom = ''
-    for i in host_bottom:
-        octetHostBottom += (str(i) + '.')
-    
+    firstIP = int(firstRange[3]) + 1
+    lastIP = int(lastRange[3]) - 1
 
-    host_top.insert(3,topIP)
-    host_top.pop()
-    octetHostTop = ''
-    for i in host_top:
-        octetHostTop += (str(i) + '.')
-    
-    print(f"Range Host      = {octetHostBottom[:-1]} - {octetHostTop[:-1]}")
-    print(f"Range IP        = {octetNet[:-1]} - {octetBroad[:-1]}")
+    # Insert the first IP for Range Host
+    firstRange.insert(3,firstIP)
+    firstRange.pop()
+    firstRangeHost = ''
+    for i in firstRange:
+        firstRangeHost += (str(i) + '.')
 
+    # Insert the last IP for Range Host
+    lastRange.insert(3,lastIP)
+    lastRange.pop()
+    lastRangeHost = ''
+    for i in lastRange:
+        lastRangeHost += (str(i) + '.')
 
+    return f"Range IP = {first} - {last}\nRange Host = {firstRangeHost[:-1]} - {lastRangeHost[:-1]}"
+RangeIP = rangeIP(network,broadcast)
 
 # Start The Calculate 
-jumlahIP = sumIP(inputSubnet)
+if inputSubnet >= 24:
+    jumlahIP = sumIP(inputSubnet)
+    subnet = subnetmaskC(jumlahIP)
+else:
+    subnet = subnetmaskB(inputSubnet)
 
 
 # The Main Function That Print The Result of Calculate
 def main():
     print("\n============================================")
     print(f"Total IP        = {jumlahIP}")
-    subnet = subnetting(jumlahIP)
-    network_broadcast_IP(inputIP)
+    print(subnet)
+    print(network)
+    print(broadcast)
+    print(RangeIP)
     print("============================================")
 
 # Run Result Function
